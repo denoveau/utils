9ddb8700d000c9ce025ab768b87bda20666c5964
Sriram Murali <sriram.velamur@gmail.com>
2025-02-17 18:37:32 +0530

    Changes relative path to use an env APPS_ROOT variable and fallback to current root.

--- deploy.js
+++ deploy.js
@@ -1,17 +1,21 @@
 import { readdir, readFile } from 'fs/promises'
+import { dirname } from 'path'
+
+const root = process.env.APPS_ROOT || await dirname(import.meta.dirname)
+
 import { exec } from 'child_process'
 const pkgName = import.meta.dirname.split('/').pop()
-await Promise.all((await readdir('../')).map(async (_) => {
+await Promise.all((await readdir(root)).map(async (_) => {
   if (_ === pkgName) return
   try {
-    let [ packageJSON ] = (await readdir(`../${_}`)).filter((_) => _ === 'package.json')
+    let [ packageJSON ] = (await readdir(`${root}/${_}`)).filter((_) => _ === 'package.json')
     if (!packageJSON) { console.log(`No package.json found in ${_}`); return }
-    packageJSON = JSON.parse((await readFile(`../${_}/package.json`)).toString())
+    packageJSON = JSON.parse((await readFile(`${root}/${_}/package.json`)).toString())
     if (!(packageJSON.dependencies || {})[pkgName]) {
       console.log(`${_} does not require ${pkgName}. Skipping.`)
       return
     }
     console.log(`Updating ${_} for ${pkgName}`)
-    await exec(`cd ../${_}; npm i`)
+    await exec(`cd ${root}/${_}; npm i`)
   } catch (_) { }
 }))
